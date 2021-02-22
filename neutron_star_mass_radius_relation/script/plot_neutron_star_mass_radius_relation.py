a5c6cc2262b3d51df82fac8f8e97b2b8e5d0a9e3
Teruaki Enoto <enoto@chromy.ad15.riken.go.jp>
2021-02-22 11:40:15 +0900

    rho-P

--- neutron_star_mass_radius_relation/script/plot_neutron_star_mass_radius_relation.py
+++ neutron_star_mass_radius_relation/script/plot_neutron_star_mass_radius_relation.py
@@ -18,7 +18,7 @@ def cm2inch(*tupl):
 mr_model_list = [
 	'APR',
 	'BL',	
-	'CMF',	
+#	'CMF',	
 #	'DD2_FRG2f',		
 	'DD2_FRG3f',
 	'SKa',	
@@ -37,16 +37,29 @@ mr_model_list = [
 
 # ------ Density vs. Pressure ------
 
-df_rhoP = pd.read_excel('data/EOS.xlsx',engine='openpyxl',
-	sheet_name=None,header=None,names=['density','n/a','pressure'])
+#df_rhoP = pd.read_excel('data/EOS.xlsx',engine='openpyxl',
+df_rhoP = pd.read_excel('data/EOS_v210221.xlsx',engine='openpyxl',
+	sheet_name=None,header=None,names=['density','n/a','pressure','n/a','energy_density'])
 df_rhoP['SLY230a'] = df_rhoP['SLY230a'].drop(index=0) # this sheet the header line 
 del df_rhoP['Sheet13'] # this is null ssheet
 
 df_Raai = pd.read_csv('data/Raaijmakers/fig2/wpd_datasets_mod.csv')
-Raai_core_x = 10**df_Raai['core-X'].dropna()/2.7e+14*0.17
+#Raai_core_x = 10**df_Raai['core-X'].dropna()/2.7e+14*0.17
+Raai_core_x = 10**df_Raai['core-X'].dropna()*5.609e-13 
 # ρ0 = 0.17 fm-3 = 2.7e+14 g/cm3
+# fm = 1e-13 cm
+# cm = 1e+13 fm
+# for electron:
+#	0.511 MeV <--> 9.109383701e-31 kg <--> 9.109383701e-28 g 
+# g/cm3 = 0.511 MeV/9.109e-28 / (1e+13 fm)^3
+#       = 0.05609 MeV * 1e-11 fm-3 
+#       = 5.609e-13 MeV fm-3
 Raai_core_y = 10**df_Raai['core-Y'].dropna()/1.60e+33
 # MeV / fm3 = 1.60e+33 dyne/cm2 (http://asphwww.ph.noda.tus.ac.jp/myn/guide.pdf)
+
+df_Raai_cont = pd.read_csv('data/Raaijmakers/fig2/wpd_datasets_Raaijmakers_fig2_leftupper.csv')
+Raai_cont_x = 10**df_Raai_cont['X'].dropna()*5.609e-13 
+Raai_cont_y = 10**df_Raai_cont['Y'].dropna()/1.60e+33
 
 plt.style.use('script/matplotlibrc.template')
 # For guidance, Nature's standard figure sizes are 89 mm wide (single column) and 183 mm wide (double column). 
@@ -58,21 +71,22 @@ ax1 = fig.add_subplot(111)
 #ax1.xaxis.set_major_formatter(mticker.ScalarFormatter())
 
 zorder = 0
-ax1.axvline(x=0.16,ls='--',color="#F39C12",zorder=zorder);zorder+=1
+#ax1.axvline(x=0.16,ls='--',color="#F39C12",zorder=zorder);zorder+=1
 
 for sheet_name in mr_model_list:
 	print(sheet_name)
-	ax1.plot(df_rhoP[sheet_name]['density'],df_rhoP[sheet_name]['pressure'],
+	ax1.plot(df_rhoP[sheet_name]['energy_density'],df_rhoP[sheet_name]['pressure'],
 		label=sheet_name,zorder=zorder)
 	zorder+=1 
 
 ax1.plot(Raai_core_x,Raai_core_y,'-')
+ax1.plot(Raai_cont_x,Raai_cont_y,'--k')
 
 #ax1.set_xlim(0.01,0.18)
 #ax1.set_ylim(0.01,2.0)
-ax1.set_xlim(0.1,1.0)
+ax1.set_xlim(50,1000)
 ax1.set_ylim(0.5,500.0)
-ax1.set_xlabel('Baryon number density (fm$^{-3}$)')
+ax1.set_xlabel('Total energy density (MeV fm$^{-3}$)')
 ax1.set_ylabel('Pressure (MeV fm$^{-3}$)')
 ax1.legend(loc='upper left',borderaxespad=1,fontsize=10,ncol=2)
 
